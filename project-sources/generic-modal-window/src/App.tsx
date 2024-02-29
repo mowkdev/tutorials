4990ece6f61811c173f05c14b9a7048d725f4783
mowkdev <143942924+mowkdev@users.noreply.github.com>
2024-02-29 02:34:07 +0200

    Improved styles and page title

--- project-sources/generic-modal-window/src/App.tsx
+++ project-sources/generic-modal-window/src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import styles from "./App.module.scss";
-import GenericModal from "./components/GenericModal/GenericModal.tsx";
-import ModalDialogBox from "./components/ModalBox/ModalDialogBox.tsx";
+import { useState } from 'react';
+import styles from './App.module.scss';
+import GenericModal from './components/GenericModal/GenericModal.tsx';
+import ModalDialogBox from './components/ModalBox/ModalDialogBox.tsx';
 
 function App() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [isDialogModalOpen, setDialogModalOpen] = useState(false);
 
   return (
-    <div className={styles["app-container"]}>
-      <div className={styles["button-container"]}>
+    <div className={styles['app-container']}>
+      <h1 className={styles['page-heading']}>Generic Modal Window</h1>
+      <div className={styles['button-container']}>
         <button onClick={() => setModalOpen(true)}>Open Modal</button>
-        <button onClick={() => setDialogModalOpen(true)}>
-          Open Dialog Modal
-        </button>
+        <button onClick={() => setDialogModalOpen(true)}>Open Dialog Modal</button>
       </div>
       <GenericModal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
         <p>This is a modal!</p>
@@ -21,10 +20,8 @@ function App() {
       <ModalDialogBox
         isOpen={isDialogModalOpen}
         onClose={() => setDialogModalOpen(false)}
-        title={"This is dialog modal"}
-        content={
-          "This modal utilizes GenericModal component and has custom styles applied."
-        }
+        title={'This is dialog modal'}
+        content={'This modal utilizes GenericModal component and has custom styles applied.'}
       />
     </div>
   );
